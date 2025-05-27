bbdf7826b71b3c30fff79eb67c48c9da5c46dd0b
Guillermo <2820945@alu.murciaeduca.es>
2025-05-27 22:32:20 +0200

    hace animación falta dialogos

--- Scenes/Escenarios/dialogo_rrhh.gd
+++ Scenes/Escenarios/dialogo_rrhh.gd
@@ -1,27 +1,31 @@
 extends Sprite2D
 
-@export var texto:String = "":
-	set(value):
-		visible= true
-		texto = value
-		$timer.start()
+@export var texto: String = ""
+var index = 0
 
-var index = 0;
+func _set_texto(value):
+	print("Mostrando texto:", value)
+	visible = true
+	texto = value
+	index = 0
+	$Label.text = ""
+	$timer.start()
 
-# Called when the node enters the scene tree for the first time.
+func _on_timer_timeout():
+	if index >= texto.length():
+		$timer_esconde.start()
+		$timer.stop()
+	else:
+		$Label.text = $Label.text + texto[index]
+		index += 1
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func _on_timer_timeout():
-	$Label.text = $Label.text + texto[index]
-	index += 1
-	pass # Replace with function bod
-
-
 func _on_timer_esconde_timeout() -> void:
-	pass # Replace with function body.
+	visible = false
+	$Label.text = ""
+	index = 0
