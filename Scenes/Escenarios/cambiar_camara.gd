1cef18582c48ca341589b5f850392bb1ff512e13
Guillermo <2820945@alu.murciaeduca.es>
2025-05-29 19:14:10 +0200

    subido para hacer cambios de plano

---
+++ Scenes/Escenarios/cambiar_camara.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export var camara_lejos: Camera2D
+@export var camara_cerca: Camera2D
+
+var camara_actual_cerca := false
+
+func _ready():
+	input_pickable = true
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("¡Clic detectado!")
+		cambiar_camara()
+
+func cambiar_camara():
+	if camara_actual_cerca:
+		camara_lejos.current = true
+		camara_cerca.current = false
+	else:
+		camara_cerca.current = true
+		camara_lejos.current = false
+	camara_actual_cerca = !camara_actual_cerca
