a9dc2c7ad2a93bc5881baaf32db11597d6a0d894
Guillermo <2820945@alu.murciaeduca.es>
2025-05-29 19:28:57 +0200

    subido

--- Scenes/Escenarios/cambiar_camara.gd
+++ Scenes/Escenarios/cambiar_camara.gd
@@ -5,13 +5,10 @@ extends Area2D
 
 var camara_actual_cerca := false
 
-func _ready():
-	input_pickable = true
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("¡Clic detectado!")
-		cambiar_camara()
+#func _on_CambiarCamara_input_event(viewport, event, shape_idx):
+	#if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		#print("¡Clic detectado!")
+		#cambiar_camara()
 
 func cambiar_camara():
 	if camara_actual_cerca:
@@ -21,3 +18,13 @@ func cambiar_camara():
 		camara_cerca.current = true
 		camara_lejos.current = false
 	camara_actual_cerca = !camara_actual_cerca
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("¡Clic detectado!")
+		cambiar_camara()
+
+func _on_cambiar_camara_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("¡Clic detectado desde hijo!")
+		cambiar_camara()
